757361fe756af67810879dfba4d5f285947461ae
Anderson Santos <adsantos@gmail.com>
2021-04-13 22:47:34 -0300

    update 1.9.2

--- scripts/.ipynb_checkpoints/call-simul-shop-tls-checkpoint.sh
+++ scripts/.ipynb_checkpoints/call-simul-shop-tls-checkpoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Configurando acesso ao Ingress
 export INGRESS_HOST=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 export INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].port}')
 export SECURE_INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="https")].port}')
@@ -8,12 +7,19 @@ export TCP_INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgatew
 
 while true
 do
-## Sem modificar o /etc/hosts
-#curl -v --header "Host: simul-shop.com" http://localhost/s
-#curl -v -H "Host: www.simul-shop.com" --resolve "www.simul-shop.com:80:127.0.0.2" http://www.simul-shop.com/s
-## Resolvendo pelo /etc/hosts
-# curl -v http://simul-shop.com/s
-curl -v -H "Host: www.simul-shop.com" http://$INGRESS_HOST:$INGRESS_PORT/s
+curl -v -H "Host: www.simul-shop.com" \
+    --resolve "www.simul-shop.com:$SECURE_INGRESS_PORT:$INGRESS_HOST" \
+    --cacert assets/exemplos/simul-shop/certs/all.simul-shop.com.crt "https://www.simul-shop.com:$SECURE_INGRESS_PORT/s"
 echo
 sleep 1
 done
+
+# TODO: Configurar certificado para reconhecer simul-shop.com
+# while true
+# do
+# curl -v -H "Host: simul-shop.com" \
+#     --resolve "simul-shop.com:$SECURE_INGRESS_PORT:$INGRESS_HOST" \
+#     --cacert assets/exemplos/simul-shop/certs/all.simul-shop.com.crt "https://simul-shop.com:$SECURE_INGRESS_PORT/s"
+# echo
+# sleep 1
+# done
